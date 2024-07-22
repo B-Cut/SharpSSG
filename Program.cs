1ccc855f3bef3005948fc74ee5b911567af5a058
B-Cut <caelgonsalvesrj@hotmail.com>
2024-07-22 14:10:28 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// This is a simple static site generator powered by .NET and C#
+
+using SharpSiteGenerator;
+
+var settings = SiteSettings.Instance;
+
+Console.WriteLine($"URL: {settings.Url}\nHome file: {settings.HomeFileName}\nTitle: {settings.Title}");
